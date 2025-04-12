9bb4cb7bbae3e638c9643387077260811327bbb0
Hansana Sandeepa <151901922+HansSandeepa@users.noreply.github.com>
2025-04-12 21:44:35 +0530

    add navigation bar to the page

--- js/navbar.js
+++ js/navbar.js
@@ -1,1 +1,19 @@
-//this is a comment
+const sidebar = document.querySelector('.sidebar'),
+    overlay = document.getElementById('overlay'),
+    closeBtn = document.getElementById('closeBtn'),
+    openBtn = document.getElementById('navbarBtn');
+
+openBtn.addEventListener('click', function () {
+    sidebar.classList.add('active');
+    overlay.classList.add('active');
+});
+
+closeBtn.addEventListener('click', function () {
+    sidebar.classList.remove('active');
+    overlay.classList.remove('active');
+});
+
+overlay.addEventListener('click', function(){
+    sidebar.classList.remove('active');
+    overlay.classList.remove('active');
+});
